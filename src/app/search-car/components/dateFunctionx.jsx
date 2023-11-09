604dddfd6418ae3498fb418bd4ff6ad23b2411cf
Fajar Rizky Widodo <frizkywidodo@gmail.com>
2023-11-09 17:57:28 +0700

    feat: change calendar in search-car page
    Here are the changes:
    1. Change the calendar in search-car page to match other page's calendar
    style. I put it inside searchBar component
    2. Make filterAndSort component and put it inside the search-car page.
    3. Change minor styling rules in globals.css

---
+++ src/app/search-car/components/dateFunctionx.jsx
@@ -0,0 +1,69 @@
+"use client";
+
+import React, { useState, useEffect, useContext } from "react";
+import {
+  RiArrowDownSLine,
+  RiArrowUpSLine,
+  RiCalendar2Fill,
+} from "react-icons/ri";
+import { Menu } from "@headlessui/react";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { DateRange } from "react-date-range";
+import { format, addDays } from "date-fns";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+
+export default function DateFunctionx() {
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  console.log(format(date[0].startDate, "dd-MM-yy"));
+  console.log(format(date[0].endDate, "dd-MM-yy"));
+
+  return (
+    <Menu as="div" className="w-full xl:max-w-[296px] cursor-pointer relative">
+      <Menu.Button
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex h-[64px] items-center px-[18px] gap-x-2 border-2 border-[#cdcdcd] rounded-lg w-full text-left"
+      >
+        <RiCalendar2Fill className="text-2xl mr-[18px] text-[#045757] " />
+        <div>
+          <div className="flex items-center gap-x-3">
+            <div className="text-[15px] font-medium leading-tight">
+              {" "}
+              {format(date[0].startDate, "dd/MM/yy")}
+            </div>
+            <FaArrowRightLong className="text-[#045757] text-[12px]" />
+            {date[0].endDate ? (
+              <div>{format(date[0].endDate, "dd/MM/yy")}</div>
+            ) : (
+              <div>{format(date[0].startDate, "dd/MM/yy")}</div>
+            )}
+          </div>
+          <div className="text-[13px]">Select Your Date</div>
+        </div>
+        {isOpen ? (
+          <RiArrowUpSLine className="text-2xl ml-auto text-[#045757] " />
+        ) : (
+          <RiArrowDownSLine className="text-2xl ml-auto text-[#045757] " />
+        )}
+      </Menu.Button>
+      <Menu.Items className="text-[15px] space-y-6 shadow-md bg-white absolute w-full z-10 list-none rounded-b-lg">
+        <DateRange
+          onChange={(item) => setDate([item.selection])}
+          editableDateInputs={true}
+          moveRangeOnFirstSelection={false}
+          ranges={date}
+          rangeColors={["#045757"]}
+          minDate={addDays(new Date(), 0)}
+        />
+      </Menu.Items>
+    </Menu>
+  );
+}
