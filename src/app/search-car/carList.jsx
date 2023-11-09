2b88fd713e5d75e0df7d2136c0cef4a7f68d19e1
Fajar Rizky Widodo <frizkywidodo@gmail.com>
2023-11-09 07:45:47 +0700

    fix: fix discount and payable issue
    Here are the changes:
    1. Fix the discount and payable issues in Booking Summary page. Before
    this commit, the page couldn't calculate the discount and payable for a
    particular car. Now, it should be able to calculate and render them.
    2. Fix styling issue in Search Car page. Before this commit, if the page
    is small, the gap between each car cards are none. Now, the page should
    render the gaps.
    3. Fix format issue in summaryCard.jsx, carList.jsx, and carCard.jsx

--- src/app/search-car/carList.jsx
+++ src/app/search-car/carList.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import CarCard from "./components/carCard";
 
 const CarList = () => {
@@ -8,8 +8,10 @@ const CarList = () => {
 
   const getCarData = async () => {
     try {
-      const res = await fetch(process.env.NEXT_PUBLIC_RB_REST_API_URL + "/api/car/all");
-      const {car} = await res.json();
+      const res = await fetch(
+        process.env.NEXT_PUBLIC_RB_REST_API_URL + "/api/car/all"
+      );
+      const { car } = await res.json();
       setCarData(car);
     } catch (err) {
       console.error(err);
@@ -20,16 +22,10 @@ const CarList = () => {
     getCarData();
   }, []);
   return (
-    <div>
-      <h1>
-        {carData.map((item, i) => {
-          return (
-            <div key={i}>
-              <CarCard item={item} />
-            </div>
-          );
-        })}
-      </h1>
+    <div className="flex flex-col gap-6 md:gap-0">
+      {carData.map((item, i) => {
+        return <CarCard key={i} item={item} />;
+      })}
     </div>
   );
 };
