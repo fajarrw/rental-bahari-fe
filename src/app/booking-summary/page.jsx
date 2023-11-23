cad88328623158d37a368ecd4d13238d87c4751f
Fajar Rizky Widodo <frizkywidodo@gmail.com>
2023-11-23 20:12:03 +0700

    fix: navbar at booking-summary page
    Here are the changes:
    1. In this commit, the booking-summary page should render the
    corresponding navbar for both logged in and logged out users.

--- src/app/booking-summary/page.jsx
+++ src/app/booking-summary/page.jsx
@@ -1,23 +1,46 @@
+"use client"
+
 import Link from "next/link";
 import SummaryCard from "./components/summaryCard";
 import { SearchContextFunction } from "../before_login/context/search";
+import { SearchContextFunctionx } from "../after_login/context/cari";
 import Header from "../before_login/components/navbar/header";
+import Headerx from "../after_login/components/navbar/headerx";
+import { useGetRole } from "@/hooks/useCookies";
+import { useState, useEffect } from "react";
 
 export default function BookingSummary() {
+  const [isUser, setIsUser] = useState(false);
+
+  const getRole = async () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const role = await useGetRole();
+    if (role?.value == "user") {
+      setIsUser(true);
+    }
+    console.log({ isUser });
+  };
+
+  useEffect(() => {
+    getRole();
+  }, []);
+
   return (
-    <SearchContextFunction>
-      <Header />
-      <main className="flex flex-col items-center pt-2 min-h-screen">
-        <div className="flex flex-row px-16 py-3 gap-3 w-full">
-          <Link href="/search-car" className="font-poppins font-medium">
-            &lt;
-          </Link>
-          <h1 className="font-poppins font-medium">Booking Summary</h1>
-        </div>
-        <div className="flex flex-row">
-          <SummaryCard />
-        </div>
-      </main>
-    </SearchContextFunction>
+    <SearchContextFunctionx>
+      <SearchContextFunction>
+        {isUser ? <Headerx /> : <Header />}
+        <main className="flex flex-col items-center pt-2 min-h-screen">
+          <div className="flex flex-row px-16 py-3 gap-3 w-full">
+            <Link href="/search-car" className="font-poppins font-medium">
+              &lt;
+            </Link>
+            <h1 className="font-poppins font-medium">Booking Summary</h1>
+          </div>
+          <div className="flex flex-row">
+            <SummaryCard />
+          </div>
+        </main>
+      </SearchContextFunction>
+    </SearchContextFunctionx>
   );
 }
