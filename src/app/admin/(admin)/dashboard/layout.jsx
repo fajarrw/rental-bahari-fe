d5bdf8750e8a56f9ea7b3c1176603ed84cadc339
PetrusAriaa <petrusariacr25@gmail.com>
2023-12-02 02:42:22 +0700

    fix: unintended page redirect issue

--- src/app/admin/(admin)/dashboard/layout.jsx
+++ src/app/admin/(admin)/dashboard/layout.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/navbar";
 
 export default function DashboardLayout({children}) {
   return (
-    <main className="h-screen bg-[#EDEDED] overflow-auto">
+    <main className="h-screen bg-[#EDEDED]">
       <Navbar />
       <section>{children}</section>
     </main>
