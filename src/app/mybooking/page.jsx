4ebb54a2b65ba9d2043172bed09e7ccad0d9e7e9
Daiva <antoniusdaiva@gmail.com>
2023-12-04 23:30:09 +0700

    fix: show message when booking not found

--- src/app/mybooking/page.jsx
+++ src/app/mybooking/page.jsx
@@ -6,6 +6,7 @@ import BookingTabs from './components/bookingTabs';
 
 function MyBookingPage() {
     return (
+<<<<<<< Updated upstream
         <div>
             
             <div className='flex pt-20 justify-center justify-items-center'>
@@ -14,9 +15,27 @@ function MyBookingPage() {
                     <BookingCard />
                     <BookingCard />
                     <BookingCard />
+=======
+        <SearchContextFunctionx>
+            <Headerx />
+            <div className='flex flex-col items-center min-h-screen pt-10'>
+                <div className='flex flex-col items-center'>
+                    <BookingTabs />
+                    {RentData && RentData.length > 0 ? (
+                    <BookingList bookings={RentData} />
+                    ) : (
+                    <p className='pt-10'>No bookings found.</p>
+                    )}
+>>>>>>> Stashed changes
                 </div>
+                <div className='flex-1w-full'></div>
             </div>
+<<<<<<< Updated upstream
         </div>
+=======
+            
+        </SearchContextFunctionx>
+>>>>>>> Stashed changes
     );
 }
 
